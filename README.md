4fb8fcc6a69610f71025a045f593a2f508a54f82
John Curry <fastpenguin91@gmail.com>
2020-08-29 10:15:25 -0700

    intro complete

---
+++ README.md
@@ -0,0 +1,7 @@
+Welcome to the GraphQL tutorial series.
+
+To get started, please do the following:
+
+$ npm install
+
+$ node src/index.js
---
+++ src/index.js
@@ -0,0 +1,19 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+const typeDefs = `
+type Query {
+  helloWorld: String!
+}
+`;
+
+const resolvers = {
+  Query: {
+    helloWorld: () => `Hello World! What a day!`,
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
