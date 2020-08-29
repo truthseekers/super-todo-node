32fc82013da363813d760821fe3253f4615fc4ad
John Curry <fastpenguin91@gmail.com>
2020-08-29 14:20:47 -0700

    starting on Queries with Arguments

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
 const { GraphQLServer } = require("graphql-yoga");
 
+let users = [
+  {
+    id: "123",
+    firstName: "Cindy",
+    email: "cindy@cindy.com",
+  },
+  {
+    id: "456",
+    firstName: "Todd",
+    email: "todd@todd.com",
+  },
+];
+
 const typeDefs = `
 type Query {
   helloWorld: String!
+  users: [User!]!
+}
+
+type User {
+    id: ID!
+    firstName: String!
+    email: String!
 }
 `;
 
 const resolvers = {
   Query: {
     helloWorld: () => `Hello World! What a day!`,
+    // users: () => users, // We could do this since it's such a simple query
+    users: (parent, args, context, info) => {
+      return users;
+    },
+  },
+  User: {
+    id: (parent) => parent.id,
+    firstName: (parent) => {
+      console.log("What is parent: ", parent);
+      return parent.firstName;
+    },
+    // firstName: (parent) => parent.firstName, // simple version. above to illustrate
+    email: (parent) => parent.email,
   },
 };
 
